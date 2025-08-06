116acc785b2146b3d41b98f07a3ec52d7f49d495
AurelinoLino <aurelino.silveira@gmail.com>
2025-08-06 14:19:43 -0300

    Certo e nao errado

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-console.log("Oi AVS")
+// Entrada nomeando 2 variaveis nome e xp (experiencia)
 
-avssss
+let nome = "Aurelino"
+
+let XP = 1000
+
+let nivel = "AVS"
+
+//Repeticao
+
+if (XP<1000) {nivel = "Ferro"
+    
+}
+
+else if (xp>1000) and (xp<=2000) 
+
+{ nivel = "Bronze"
+    
+} 
+
+else if (xp>2001) and (xp<=5000)
+
+{nivel = "Prata"}
+
+else {"Vc esta fora do nivel"}
+
+// 50001 7000 Ouro
+// 7001 8000 Platina
+// 8001 9000 Ascendente
+// 9001 10000 Imortal
+// 10001 Radiante
+
+
+// Saida
+
+console.log("O Herói de nome " + nome + "está no nivel de " + nivel)
